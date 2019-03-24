83e444c41b30c93a3c73b06141c6014cc6e9134e
ChiaEnChiang <vivian123.cs02@gmail.com>
2019-03-24 19:14:39 +0100

    add new features of webpack

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,1 +1,20 @@
-console.log("app.js is cool running");
+//import "./utils.js";
+import ReactDOM from "react-dom";
+import React from "react";
+
+const template = <p>test123</p>;
+ReactDOM.render(template, document.getElementById("app"));
+
+// loader
+
+// for name export and default export practice
+// import subtract, { square, add } from "./utils.js";
+// import isSenior, { isAdult, canDrink } from "./person.js";
+
+// console.log("app.js is cool running");
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 81));
+// console.log(isSenior(22));
+// console.log(isAdult(17));
+// console.log(canDrink(22));
