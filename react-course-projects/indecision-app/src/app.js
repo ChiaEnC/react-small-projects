494a17e039c7286b1102a576d7c8fbceabb16315
ChiaEnChiang <vivian123.cs02@gmail.com>
2019-03-25 23:43:19 +0100

    styling react

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -2,4 +2,6 @@ import ReactDOM from "react-dom";
 import React from "react";
 import IndecisionApp from "./components/IndecisionApp";
 
+import "./styles/styles.scss";
+import "normalize.css/normalize.css";
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
--- react-course-projects/indecision-app/src/components/OptionModal.js
+++ react-course-projects/indecision-app/src/components/OptionModal.js
@@ -6,10 +6,16 @@ const OptionModal = props => (
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
     onRequestClose={props.handleClearSelectedOption}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h1>Seleted Option </h1>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Seleted Option </h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.handleClearSelectedOption}>
+      Okay
+    </button>
   </Modal>
 );
 
