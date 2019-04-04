939b1e36f56d3e710c310d1e1a8fd62105d4691d
ChiaEnChiang <vivian123.cs02@gmail.com>
2019-04-04 12:03:45 +0200

    add hoc

--- react-course-projects/expenisify-app/src/app.js
+++ react-course-projects/expenisify-app/src/app.js
@@ -10,10 +10,9 @@ import "normalize.css/normalize.css";
 const store = configureStore();
 
 store.dispatch(addExpense({ description: "Water bill" }));
-store.dispatch(
-  addExpense({ description: "Gas bill", amount: 22, createAt: 100 })
-);
-store.dispatch(setTextFilter("water"));
+store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(setTextFilter("bill"));
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
